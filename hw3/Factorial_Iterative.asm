d7b5b1635411f4c5f9d2053b9b7926b50da0e994
Andrew Jong <andrewjong87@gmail.com>
2017-03-19 20:25:26 -0700

    Added hw3 starter files

---
+++ hw3/Factorial_Iterative.asm
@@ -0,0 +1,23 @@
+.include "./cs47_macro.asm"
+
+.data
+msg1: .asciiz "Enter a number ? "
+msg2: .asciiz "Factorial of the number is "
+charCR: .asciiz "\n"
+
+.text
+.globl main
+main:	print_str(msg1)
+	read_int($t0)
+	
+# Write body of the iterative
+# factorial program here
+# Store the factorial result into 
+# register $s0
+	
+	print_str(msg2)
+	print_reg_int($s0)
+	print_str(charCR)
+	
+	exit
+	
